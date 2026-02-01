926eecdbb9ce5d8b58b29de388cb5e15c5579530
Siegfriedz37 <51910320+crabadon17@users.noreply.github.com>
2026-02-01 03:46:55 +0000

    Add terraform demo

---
+++ docker-terraform/terraform/terraform-demo/variables.tf
@@ -0,0 +1,34 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "/workspaces/DataTalks-Data-Engineering-Zoomcamp/docker-terraform/terraform/keys/my-creds.json"
+}
+
+variable "project" {
+  description = "Project"
+  default     = "de-zoomcamp-486100"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "de-zoomcamp-486100-terra-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
